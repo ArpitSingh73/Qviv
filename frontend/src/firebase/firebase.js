631fad790bfffea63c3a4d3fe165300cb41cec6b
ArpitSingh73 <arpitsingh73073@gmail.com>
2024-05-05 18:53:24 +0530

    API keys encryption.

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -1,24 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC28bQKYt7utqFMFrQvuPaJo4PkY0Qcvjw",
-  authDomain: "qviv-8aafe.firebaseapp.com",
-  projectId: "qviv-8aafe",
-  storageBucket: "qviv-8aafe.appspot.com",
-  messagingSenderId: "1065875189122",
-  appId: "1:1065875189122:web:a950653bce61641691739b",
-  measurementId: "G-GXM4785FSM",
+  apiKey: process.env.REACT_APP_google_api_key,
+  authDomain: REACT_APP_google_auth_doamin,
+  projectId: REACT_APP_google_project_id,
+  storageBucket: REACT_APP_google_storage_bucket,
+  messagingSenderId: process.env.REACT_APP_google_message_sender_id,
+  appId: process.env.REACT_APP_google_auth_domain,
+  measurementId: process.env.REACT_APP_google_measurement_id,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 
-export {app, auth}
+export { app, auth };
